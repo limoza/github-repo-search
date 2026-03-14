3a7f6aa4c06531f45b6f014f7b4deb49392a37d2
limoza <svacc.nelllely@gmail.com>
2026-03-15 01:05:47 +0900

    テストもそれに合わせて一旦修正

--- src/app/page.test.tsx
+++ src/app/page.test.tsx
@@ -1,9 +1,17 @@
 import { render, screen } from '@testing-library/react';
-import Home from './page';
+import Page from './page';
 
 describe('Home', () => {
-  it('renders without crashing', () => {
-    render(<Home />);
+  it('renders without crashing', async () => {
+    const ui = await Page({
+      searchParams: Promise.resolve({
+        q: 'test',
+        page: '1',
+        sort: 'stars',
+      }),
+    });
+
+    render(ui);
     expect(screen.getByRole('main')).toBeInTheDocument();
   });
 });
