ba77efe4be4288d99f0a2374fb5bed43dcd5c32b
limoza <svacc.nelllely@gmail.com>
2026-03-21 23:04:33 +0900

    lintの実行

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,10 @@
-import { Geist } from "next/font/google";
+import { Geist } from 'next/font/google';
 import type { ReactNode } from 'react';
 
 import { AppHeader } from '@/components/AppHeader';
-import { cn } from "@/lib/utils";
+import { cn } from '@/lib/utils';
 
-const geist = Geist({subsets:['latin'],variable:'--font-sans'});
-
+const geist = Geist({ subsets: ['latin'], variable: '--font-sans' });
 
 type RootLayoutProps = {
   children: ReactNode;
@@ -13,7 +12,7 @@ type RootLayoutProps = {
 
 export default function RootLayout({ children }: RootLayoutProps) {
   return (
-    <html lang="ja" className={cn("font-sans", geist.variable)}>
+    <html lang="ja" className={cn('font-sans', geist.variable)}>
       <body>
         <div>
           <AppHeader />
